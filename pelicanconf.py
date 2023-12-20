166ea509202d2c30c744e9ce7dadb19ea502bb47
46907 <4690734@qq.com>
2023-12-20 21:37:29 +0800

    init

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = '简单智能'
+SITENAME = '简单智能'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("淘宝店", "https://shop475362757.taobao.com/"),
+    ("京ICP备20001819号-3", "https://beian.miit.gov.cn/"),
+)
+
+# Social widget
+SOCIAL = (
+    # ("淘宝店", "https://shop475362757.taobao.com/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
